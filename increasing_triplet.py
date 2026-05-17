60e3d3b89b2b3f86925f0c63f916f1a4a8414a31
Jananishanmugam <82532414+Jananishanmugam@users.noreply.github.com>
2026-05-17 16:27:38 +0530

    Update increasing_triplet.py

--- increasing_triplet.py
+++ increasing_triplet.py
@@ -92,3 +92,23 @@ else :
     if (sumEven == sumOdd - A[n - 1]) :
         res+=1
 print(res)
+
+
+
+#Leet code problem
+
+class Solution:
+    def increasingTriplet(self, nums):
+        min1 = float('inf')
+        min2 = float('inf')
+        for n in nums:
+            if n <= min1:
+                min1 = n  # Update first minimum
+            elif n <= min2:
+                min2 = n  # Update second minimum
+            else:
+                return True  # Found a third number greater than both
+        return False  # No triplet found
+
+
+# Link - (https://www.youtube.com/watch?v=uYRT3gMYDwE)
